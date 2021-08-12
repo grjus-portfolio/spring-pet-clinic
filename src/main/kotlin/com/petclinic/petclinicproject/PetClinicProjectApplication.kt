67d26338547a3a575ce0d614857a0c989d05ea20
grjus <grjus@pm.me>
2021-08-12 11:11:08 +0200

    Initial commit. Spring initializer

---
+++ src/main/kotlin/com/petclinic/petclinicproject/PetClinicProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.petclinic.petclinicproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetClinicProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetClinicProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/petclinic/petclinicproject/PetClinicProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.petclinic.petclinicproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetClinicProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
