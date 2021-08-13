08a957782c16cb1c98686ece42bc23f943c4939c
grjus <grjus@pm.me>
2021-08-13 14:59:09 +0200

    Adeded owner data

--- src/main/kotlin/com/petclinic/petclinicproject/model/Vet.kt
+++ src/main/kotlin/com/petclinic/petclinicproject/model/Vet.kt
@@ -3,4 +3,8 @@ package com.petclinic.petclinicproject.model
 class Vet(firstName:String, lastName:String):Person(firstName, lastName) {
 
     lateinit var specialities:Set<Speciality>
+    lateinit var city:String
+    lateinit var adress:String
+    lateinit var phone:String
+
 }
