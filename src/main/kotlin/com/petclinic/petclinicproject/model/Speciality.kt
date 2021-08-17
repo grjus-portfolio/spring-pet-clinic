d88195fbde61a1971a79310ea13caf2ae58f9275
grjus <grjus@pm.me>
2021-08-17 10:54:42 +0200

    Converts Vets to JPA Entities

--- src/main/kotlin/com/petclinic/petclinicproject/model/Speciality.kt
+++ src/main/kotlin/com/petclinic/petclinicproject/model/Speciality.kt
@@ -1,5 +1,12 @@
 package com.petclinic.petclinicproject.model
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name="specialities")
 class Speciality:BaseEntity() {
+    @Column(name="description")
     var description:String=""
 }
--- src/main/kotlin/com/petclinic/petclinicproject/model/Vet.kt
+++ src/main/kotlin/com/petclinic/petclinicproject/model/Vet.kt
@@ -1,7 +1,13 @@
 package com.petclinic.petclinicproject.model
 
+import javax.persistence.*
+
+@Entity
+@Table(name="vets")
 class Vet(firstName:String, lastName:String):Person(firstName, lastName) {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name="vet_specialities", joinColumns = [JoinColumn(name="vet_id")], inverseJoinColumns = [JoinColumn(name="speciality_id")])
     var specialities:MutableSet<Speciality> = HashSet<Speciality>()
     var city:String = ""
     var adress:String = ""
