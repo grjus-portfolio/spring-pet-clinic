cc62f288b539e6410f08f8b21118cd120d0c0691
grjus <grjus@pm.me>
2021-08-17 10:04:13 +0200

    Added mapped super class to base entity class

--- src/main/kotlin/com/petclinic/petclinicproject/model/BaseEntity.kt
+++ src/main/kotlin/com/petclinic/petclinicproject/model/BaseEntity.kt
@@ -1,8 +1,15 @@
 package com.petclinic.petclinicproject.model
 
 import java.io.Serializable
-import kotlin.properties.Delegates
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
 
+
+@MappedSuperclass
 open class BaseEntity :Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id:Long = 1L
 }
